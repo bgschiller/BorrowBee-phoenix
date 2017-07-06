41ae2643de28dfcc653822192fd8d4c1830179bf
Brian Schiller <bgschiller@gmail.com>
2017-07-06 14:15:10 -0600

    add some more session tests

--- test/controllers/session_controller_test.exs
+++ test/controllers/session_controller_test.exs
@@ -65,5 +65,54 @@ defmodule BorrowBee.SessionControllerTest do
     assert mail.text =~ user_session_url(conn, :show, user, login_token.token)
   end
 
+  test "Login link can only be used once" do
+    user = insert(:user)
+    conn = build_conn() |> init_test_session(%{})
+
+    conn = post conn, session_path(conn, :create), user: %{"email" => user.email}
+    assert redirected_to(conn) == page_path(conn, :index)
+
+    # This line does nothing but slow us down (I think)
+    Repo.all(from x in BorrowBee.LoginToken)
+
+    login_token = Repo.get_by(BorrowBee.LoginToken, user_id: user.id)
+    assert login_token
+
+    refute get_session(conn, :current_user)
+    conn = get conn, user_session_url(conn, :show, user, login_token.token)
+    assert get_session(conn, :current_user)
+
+    conn2 = build_conn() |> init_test_session(%{})
+    conn2 = get conn2, user_session_url(conn, :show, user, login_token.token)
+
+    assert get_flash(conn2, :error) == "Access token not found or expired"
+    assert redirected_to(conn2) == page_path(conn2, :index)
+    refute get_session(conn2, :current_user)
+  end
+
+  test "Login link expires after 15 minutes" do
+    user = insert(:user)
+    conn = build_conn() |> init_test_session(%{})
+
+    conn = post conn, session_path(conn, :create), user: %{"email" => user.email}
+    assert redirected_to(conn) == page_path(conn, :index)
+
+    # This line does nothing but slow us down (I think)
+    Repo.all(from x in BorrowBee.LoginToken)
+
+    login_token = Repo.get_by(BorrowBee.LoginToken, user_id: user.id)
+    assert login_token
+
+    Ecto.Adapters.SQL.query(Repo, """
+UPDATE login_tokens SET
+  inserted_at = inserted_at - interval '16 minute'
+WHERE id = $1
+RETURNING *
+    """, [login_token.id])
+
+    refute get_session(conn, :current_user)
+    conn = get conn, user_session_url(conn, :show, user, login_token.token)
+    refute get_session(conn, :current_user)
+  end
 
 end
